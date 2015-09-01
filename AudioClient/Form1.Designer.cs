362daae0a85180313a4c3127bdc6023c9a70300a
andrewjordan <drewjordan@live.com>
2015-08-31 21:00:43 -0400

    initial add

---
+++ AudioClient/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace AudioClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 210);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(234, 92);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 309);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Click here to send to server";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(567, 13);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(481, 556);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 588);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1036, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(234, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(13, 40);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(234, 21);
+            this.comboBox2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1060, 623);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ AudioClient/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace AudioClient
+{
+    public partial class Form1 : Form
+    {
+        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+        BackgroundWorker bw = new BackgroundWorker();
+        private string currentFile = "";
+        int bytesRead = 0;
+        private int totalLength = 0;
+        private Dictionary<string,string> hosts = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            Load += Form1_Load;
+            button1.Click += button1_Click;
+            bw.DoWork += bw_DoWork;
+            bw.ProgressChanged += bw_ProgressChanged ;
+            bw.RunWorkerCompleted += BwOnRunWorkerCompleted;
+            bw.WorkerReportsProgress = true;
+        }
+
+        private void BwOnRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs runWorkerCompletedEventArgs)
+        {
+            progressBar1.Value = progressBar1.Maximum;
+        }
+
+        private void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.PerformStep();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.View = View.SmallIcon;
+            msg("Client Started");
+
+            var x = ConfigurationManager.AppSettings;
+
+            foreach (NameValueConfigurationElement v in x)
+            {
+                // get server address and port info from config file
+                hosts.Add(v.Name,v.Value);
+            }
+
+            try
+            {
+                clientSocket.Connect(hosts["defaulthost"], Convert.ToInt32(hosts["defaultport"]));
+            }
+            catch (SocketException ex)
+            {
+                DialogResult r = MessageBox.Show("Couldn't Connect! Please add a valid host and port.");
+                this.Close();
+            }
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!clientSocket.Connected)
+                return;
+            NetworkStream serverStream = clientSocket.GetStream();
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(textBox2.Text + "$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+            byte[] inStream = new byte[65536];
+
+            int counter = 0;
+            while (!serverStream.DataAvailable)
+            {
+                Thread.Sleep(50);
+                counter++;
+                if (counter > 50)
+                    return;
+            }
+            serverStream.Read(inStream, 0, (int)clientSocket.ReceiveBufferSize);
+            string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+            msg(returndata);
+            textBox2.Text = "";
+            textBox2.Focus();
+        }
+
+        public void msg(string mesg)
+        {
+            listView1.Items.Clear();
+
+            string[] list = mesg.Split(';');
+
+            foreach (var s in list)
+            {
+                listView1.Items.Add(s);
+            }
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            var s = (ListView) sender;
+            if (s.SelectedItems.Count <= 0)
+                return;
+
+            if (!clientSocket.Connected)
+                return;
+
+            GetLength(s.SelectedItems[0].Text);
+            currentFile = s.SelectedItems[0].Text;
+
+            bw.RunWorkerAsync();
+
+            
+        }
+
+        void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Read(currentFile);
+        }
+
+        void GetLength(string fileName)
+        {
+            NetworkStream serverStream = clientSocket.GetStream();
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes("gl" + fileName + "$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+
+
+            int counter = 0;
+            while (!serverStream.DataAvailable)
+            {
+                Thread.Sleep(50);
+                counter++;
+                if (counter > 50)
+                    return;
+            }
+
+            byte[] inStream;
+            using (var stream = new MemoryStream())
+            {
+                byte[] buffer = new byte[2048]; // read in chunks of 2KB
+                int bytesRead;
+                serverStream.Read(buffer, 0, buffer.Length);
+
+                //byte[] result = stream.ToArray();
+                inStream = buffer;
+            }
+            string s = Encoding.ASCII.GetString(inStream);
+            totalLength = Convert.ToInt32(s);
+
+            progressBar1.Maximum = totalLength;
+            progressBar1.Step = 2048*100;
+            progressBar1.Value = 0;
+        }
+
+        void Read(string fileName)
+        {
+            NetworkStream serverStream = clientSocket.GetStream();
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes("pl" + fileName + "$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+
+
+            int counter = 0;
+            while (!serverStream.DataAvailable)
+            {
+                Thread.Sleep(500);
+                counter++;
+                if (counter > 100)
+                    return;
+            }
+
+            byte[] inStream = new byte[99999999];
+
+            try
+            {
+                using (var stream = new MemoryStream())
+                {
+                    
+                    bytesRead = 0;
+                    int totalRead = 0;
+                    int pRead = 0;
+                    int toRead = totalLength;
+
+                    while (toRead > 0)
+                    {
+                        byte[] buffer = new byte[totalLength]; 
+                        int n = serverStream.Read(buffer, bytesRead, toRead);
+                        // The end of the file is reached.
+                        if (n == 0)
+                        {
+                            break;
+                        }
+
+                        stream.Write(buffer,bytesRead,n);
+                        bytesRead += n;
+                        bw.ReportProgress(bytesRead);
+                        toRead -= n;
+                    }
+
+
+                    byte[] result = stream.ToArray();
+                    inStream = result;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+            using (MemoryStream ms = new MemoryStream(inStream))
+            {
+                // Construct the sound player
+                SoundPlayer player = new SoundPlayer(ms);
+                MediaPlayer m = new MediaPlayer(ms.ToArray());
+                m.Play();
+            }
+        }
+    }
+
+    class MediaPlayer
+    {
+        System.Media.SoundPlayer soundPlayer;
+        public MediaPlayer(byte[] buffer)
+        {
+            MemoryStream memoryStream = new MemoryStream(buffer, true);
+            soundPlayer = new System.Media.SoundPlayer(memoryStream);
+        }
+        public void Play() { soundPlayer.Play(); }
+        public void Play(byte[] buffer)
+        {
+            soundPlayer.Stream.Seek(0, SeekOrigin.Begin);
+            soundPlayer.Stream.Write(buffer, 0, buffer.Length);
+            soundPlayer.Play();
+        }
+    }
+}
